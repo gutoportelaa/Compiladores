ec8157cd3ed2c3c6ee62770c2f0789a369d237d0
Guto <128807987+gutoportelaa@users.noreply.github.com>
2025-04-05 17:50:51 -0300

    função de transformar texto em token

--- analexico.py
+++ analexico.py
@@ -12,4 +12,23 @@ token_rules = [             #tupla de expressões regulares e seus nomes
     ('TEXTO', r'[^\n]+'),
     ('NEWLINE', r'\n'),
     ('SKIP', r'[ \t]+'),
-]
+]                                   #reparem que não tem OCORRENCIA, REGISTRO ou ENVOLVIDOS. Isso é pq são símbolos não terminais
+                                       #logo, não se encaixam como tokens. Eles são gerados na hora de criar a árvore sintática
+
+def tokenizar(texto):
+    tokens = []
+    pos = 0
+    while pos < len(texto):
+        match = None
+        for token_tipo, pattern in token_rules:
+            regulexpress = re.compile(pattern)
+            match = regulexpress.match(texto, pos)
+            if match:
+                if token_tipo != 'SKIP' and token_tipo != 'NEWLINE':
+                    tokens.append((token_tipo, match.group(0).strip()))
+                pos = match.end()
+                break
+        if not match:
+            raise SyntaxError(f"Erro léxico próximo de: {texto[pos:]}")
+    return tokens 
+                    
